8866bc35ed0c0ea990c36a298e66953f30c04a0b
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2025-07-13 11:59:17 +0300

    Add files via upload

---
+++ Codeforces/Assiut/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,66 @@
+// ﷽
+// Contest: Sheet #1 (Data type - Conditions)
+// Judge: Codeforces
+// URL: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+// Memory Limit: 256
+// Time Limit: 1000
+// Start: Sat Jul  5 18:08:42 2025
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define OO 2e9
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int a;
+  cin >> a;
+  long long b;
+  cin >> b;
+  char c;
+  cin >> c;
+  float d;
+  cin >> d;
+  double e;
+  cin >> e;
+  cout << a << endl << b << endl << c << endl << d << endl << e << endl;
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
