0065b84d969306a217b78dd58ab0290e6c7a4119
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2025-07-13 12:04:14 +0300

    Create A. The Fool.cpp

---
+++ Codeforces/Mansoura/Level Zero 2025 || Online/Contest One/A. The Fool.cpp
@@ -0,0 +1,60 @@
+// ﷽
+// Contest: Contest One
+// Judge: Codeforces
+// URL: https://codeforces.com/group/XcpeywXZh9/contest/593212/problem/A
+// Memory Limit: 256
+// Time Limit: 1000
+// Start: Fri Jul  4 15:46:33 2025
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define OO 2e9
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cout << "Adham Is The Smartest" << endl;
+  }
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
