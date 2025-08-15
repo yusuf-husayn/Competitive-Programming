56bab86e750f8c1fb6c4122aba04b9e9f7eb3c51
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2025-08-15 23:57:28 +0300

    Add files via upload

---
+++ Codeforces/BSU/ICPC FCAI BSU Community 2025 - Level 0/Week 2 (conditions)/B. Which quadrant_.cpp
@@ -0,0 +1,65 @@
+// ﷽
+// Contest: Week 2 (conditions)
+// Judge: Codeforces
+// URL: https://codeforces.com/group/PwetbLUszb/contest/560556/problem/B
+// Memory Limit: 256
+// Time Limit: 1000
+// Start: Sat Aug  9 22:31:20 2025
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define OO 2e9
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int x, y;
+  cin >> x >> y;
+  if (x > 0 && y > 0)
+    cout << 1 << endl;
+  else if (x < 0 && y > 0)
+    cout << 2 << endl;
+  else if (x < 0 && y < 0)
+    cout << 3 << endl;
+  else if (x > 0 && y < 0)
+    cout << 4 << endl;
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
