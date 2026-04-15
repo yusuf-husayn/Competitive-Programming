97b0374928bf446d945e2d364bcad06579cc6b64
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-04-15 20:59:15 +0200

    Add files via upload

---
+++ Codeforces/Mansoura/Level Zero 2025 || Online/Week One/Contest One/E. Maaaaaaaaaaaaaaaaaath.cpp
@@ -0,0 +1,60 @@
+// ﷽
+// Contest: Contest One
+// Judge: Codeforces
+// URL: https://codeforces.com/group/vtfsyP8jkN/contest/593212/problem/E
+// Memory Limit: 256
+// Time Limit: 500
+// Start: Wed Apr 15 06:30:16 2026
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+inline void fastio() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+
+const int OO = 4e18;
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int n; cin >> n;
+  int S1 {(n * (n + 1) * (2 * n + 1)) / 6}, S2 {(n * (n + 1)) / 2};
+    cout << (S2 * S2) - S1 << endl;
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
