628aeecab9494b69f22c31b680e33eaf3b94560f
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-04-21 17:33:57 +0200

    Add files via upload

---
+++ Codeforces/Contests/Educational Codeforces Round 189 (Rated for Div. 2)/A. A Number Between Two Others.cpp
@@ -0,0 +1,60 @@
+// ﷽
+// Contest: Educational Codeforces Round 189 (Rated for Div. 2)
+// Judge: Codeforces
+// URL: https://codeforces.com/contest/2225/problem/A
+// Memory Limit: 512
+// Time Limit: 2000
+// Start: 21/04/2026 @ 04:36:37 PM
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+inline void fastio() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+
+const int OO = 4e18;
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int x, y, z; cin >> x >> y;
+
+    cout << ((y == x * 2) ? "NO" : "YES") << endl;
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
