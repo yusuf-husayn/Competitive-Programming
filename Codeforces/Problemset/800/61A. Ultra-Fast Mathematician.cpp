197680794d8cc0ab4fc303620b7a26805a0356ae
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-08-10 11:05:48 +0300

    Add files via upload

---
+++ Codeforces/Problemset/800/61A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,61 @@
+// ﷽
+// Contest: Codeforces Beta Round 57 (Div. 2)
+// Judge: Codeforces
+// URL: https://codeforces.com/problemset/problem/61/A
+// Memory Limit: 256
+// Time Limit: 2000
+// Start: 22/05/2026 @ 10:18:36 AM
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+inline void fastio() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+
+const int OO = 4e18;
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+
+template <typename T> ostream &operator<<(ostream &output, const vector<T> &data) {
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  string s1, s2; cin >> s1 >> s2;
+
+  for(int i = 0; i < (int)s1.length(); i++) cout << ((s1[i] != s2[i]) ? 1 : 0);
+  cout << endl;
+}
+
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
