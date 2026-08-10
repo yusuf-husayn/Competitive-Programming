958522fc04f25c9856c9c17392dac5f7b03a82f8
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-08-10 11:06:35 +0300

    Add files via upload

---
+++ Codeforces/Problemset/800/110A. Nearly Lucky Number.cpp
@@ -0,0 +1,65 @@
+// ﷽
+// Contest: Codeforces Beta Round 84 (Div. 2 Only)
+// Judge: Codeforces
+// URL: https://codeforces.com/problemset/problem/110/A
+// Memory Limit: 256
+// Time Limit: 2000
+// Start: 14/05/2026 @ 09:57:03 PM
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+inline void fastio() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+
+const int OO = 4e18;
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+
+template <typename T> ostream &operator<<(ostream &output, const vector<T> &data) {
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  string s; cin >> s;
+
+  int cnt {0};
+  for(char c : s){
+    if(c == '4' || c == '7') cnt++;
+  }
+
+  cout << ((cnt == 4 || cnt == 7) ? "YES" : "NO") << endl;
+}
+
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
