9c32a711a56ccba57cf97c4b27899b54cbe10831
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-09-02 21:42:43 +0300

    Add files via upload

---
+++ Codeforces/Problemset/800/1703A. YES or YES_.cpp
@@ -0,0 +1,60 @@
+// ﷽
+// Contest: Codeforces Round 806 (Div. 4)
+// Judge: Codeforces
+// URL: https://codeforces.com/problemset/problem/1703/A
+// Memory Limit: 256
+// Time Limit: 1000
+// Start: 02/09/2026 @ 09:36:50 PM
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef JOE
+// #include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+inline void fastio() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+
+const int OO = 4e18;
+const int MOD = 1e9 + 7;
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+
+template <typename T> ostream &operator<<(ostream &output, const vector<T> &data) {
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  string s; cin >> s;
+  if(s == "YES" || s == "yES" || s == "Yes" || s == "yes" || s == "YeS" || s == "yEs" || s == "YEs" || s == "yeS") cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
+
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
